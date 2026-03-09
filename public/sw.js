09535aa3c592d9f46f4144d3916b2d281df74675
lucces53 <149671640+lucces53@users.noreply.github.com>
2026-03-09 16:54:36 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,44 @@
-const CACHE = 'repas-v1'
-const ASSETS = ['/', '/manifest.json']
+const CACHE = 'repas-v2'
+
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dashboard-restaurateur.html',
+  '/manifest.json'
+]
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  )
   self.skipWaiting()
 })
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE)
+          .map(k => caches.delete(k))
+      )
     )
   )
 })
 
 self.addEventListener('fetch', e => {
+
+  // on ignore les appels API
   if (e.request.url.includes('/api/')) return
+
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then(res => {
+        return res
+      })
+      .catch(() => {
+        return caches.match(e.request)
+      })
   )
+
 })
